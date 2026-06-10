737d6d6721672badf2788944e33182ab68cfdfe3
Miroslav Hasák <63903956+Miroslavhasak@users.noreply.github.com>
2026-06-10 15:45:14 +0200

    added files

---
+++ trading_db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    ticker VARCHAR(10) NOT NULL,
+    quantity INT NOT NULL,
+    limit_price NUMERIC(10, 2) NOT NULL,
+    status VARCHAR(20) DEFAULT 'PENDING' NOT NULL,
+    idempotency_key VARCHAR(50) UNIQUE NOT NULL,
+    version INT DEFAULT 0 NOT NULL
+)
